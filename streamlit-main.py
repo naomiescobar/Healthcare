7728f13239d0cddb1f3e365e960fb3708ff9637c
naomiescobar <98779524+naomiescobar@users.noreply.github.com>
2024-05-01 13:25:31 -0400

    Add files via upload

--- streamlit-main.py
+++ streamlit-main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import altair as alt
 
 # graph 1
 url_graph1 = "https://raw.githubusercontent.com/naomiescobar/Healthcare/streamlit_main.py/healthcare-expenditure%20per%20capita%20by%20State.csv"
@@ -15,12 +15,18 @@ filtered_data_graph1 = data_graph1[data_graph1['State'].isin(selected_states)]
 # bar chart
 if len(selected_states) > 0:
     st.title("Healthcare Expenditure per Capita Comparison")
-    
-    fig = px.bar(filtered_data_graph1, x='State', y='Health Spending per Capita', color='State', title='Healthcare Expenditure per Capita by State')
-    fig.update_layout(xaxis_title='State', yaxis_title='Healthcare Expenditure per Capita')
+
+    fig = alt.Chart(filtered_data_graph1).mark_bar().encode(
+        x='State',
+        y='Health Spending per Capita',
+        color='State'
+    ).properties(
+        width=800,
+        height=500
+    )
     
     # show the chart
-    st.plotly_chart(fig)
+    st.altair_chart(fig, use_container_width=True)
 
 # graph 2
 url_graph2 = "https://storage.googleapis.com/scsu-data-science/health-expenditure-2000-2020.csv"
@@ -32,11 +38,28 @@ selected_countries = st.sidebar.multiselect("Choose countries", data_graph2['Cou
 # menu to select countries
 filtered_data_graph2 = data_graph2[data_graph2['Country Name'].isin(selected_countries)]
 
+# columns representing the years
+year_columns = [str(year) for year in range(2000, 2024)]
+
+# melt the data to have 'Year' as a variable
+melted_data_graph2 = filtered_data_graph2.melt(id_vars=['Country Name'], value_vars=year_columns, var_name='Year', value_name='Health Expenditure (per capita)')
+
+# Convert 'Year' to int
+melted_data_graph2['Year'] = melted_data_graph2['Year'].astype(int)
+
 # line chart
 if len(selected_countries) > 0:
     st.title("Health Expenditure Comparison")
-    fig = px.line(filtered_data_graph2, x='Year', y='Health Expenditure (per capita)', color='Country Name')
-    st.plotly_chart(fig)
+    chart = alt.Chart(melted_data_graph2).mark_line().encode(
+        x='Year:O',
+        y='Health Expenditure (per capita)',
+        color='Country Name'
+    ).properties(
+        width=800,
+        height=500
+    ).interactive()
+
+    st.altair_chart(chart, use_container_width=True)
 
 # graph 3
 url_graph3 = "https://raw.githubusercontent.com/naomiescobar/Healthcare/main/us-healthcare-expenditure%20private%20vs.%20public%20(GDP).csv"
@@ -56,5 +79,12 @@ if option_public:
 if selected_data:
     st.subheader("Health Expenditure Comparison")
     melted_data_graph3 = us_data.melt(id_vars=['Year'], value_vars=selected_data, var_name='Data Type', value_name='Expenditure')
-    fig = px.line(melted_data_graph3, x='Year', y='Expenditure', color='Data Type')
-    st.plotly_chart(fig)
+    chart = alt.Chart(melted_data_graph3).mark_line().encode(
+        x='Year:O',
+        y='Expenditure',
+        color='Data Type'
+    ).properties(
+        width=800,
+        height=500
+    ).interactive()
+    st.altair_chart(chart, use_container_width=True)
